16fe811dc935cf82d2d29419d0c64df59e8baea1
tlam <tlam@lnx-tlam>
2011-01-15 11:33:29 -0500

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpendALot::Application.config.secret_token = '71cb69a05ab9546a19a0da79ac4038007793f10a499703294d73cb86b91d7235e53bee3eb688e9f7ee6f94558376fe25a3744d5a523a60a11a024c1107f24628'
